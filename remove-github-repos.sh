7e7f06aac2a8d153c967ba4df46c6bd4377b0472
nildaifn <93961893+nildaifn@users.noreply.github.com>
2021-11-09 14:03:19 +0700

    Update remove-github-repos.sh

--- remove-github-repos.sh
+++ remove-github-repos.sh
@@ -1,7 +1,7 @@
 # ⛓项目永久链接：https://github.com/itzhangbao/remove-repos
 # 将 DELETE_KOKEN 和 GithubName 都替换为自己的
-DELETE_KOKEN="33ede4e961e574dfdb204ece0ef20309756cbc9e"
-GithubName="itzhangbao"
+DELETE_KOKEN= ${{ secrets.G_TOKEN }}
+GithubName="nildaifn"
 
 for repName in $(cat repos.txt)
 do
